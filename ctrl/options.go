a7d62d12e6939b239438988044f0b6b3fe19f942
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2022-04-15 10:47:28 +0200

    Adding options for KMutators and OMutators

--- ctrl/options.go
+++ ctrl/options.go
@@ -3,6 +3,20 @@ package ctrl
 // Option is a function that sets an option in a KustCtrl controller.
 type Option func(*KustCtrl)
 
+// WithKMutator register a Kustomization mutator into the controller
+func WithKMutator(mutator KMutator) Option {
+	return func(k *KustCtrl) {
+		k.KMutators = append(k.KMutators, mutator)
+	}
+}
+
+// WithOMutator register an Object mutator into the controller
+func WithOMutator(mutator OMutator) Option {
+	return func(k *KustCtrl) {
+		k.OMutators = append(k.OMutators, mutator)
+	}
+}
+
 // WithResourceToObjectFn sets up a different translation function to be used when converting a
 // resource.Resource into a client.Object.
 func WithResourceToObjectFn(fn ResourceToObjectFn) Option {
